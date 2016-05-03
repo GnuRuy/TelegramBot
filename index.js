35658717082d340d4f025cb39c2bd616904901f4
Rodolfo Silva <contato@rodolfosilva.com>
2016-05-03 15:52:40 -0300

    Adicionando express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var TelegramBot = require('node-telegram-bot-api');
+var express = require('express');
 var config = require('./config');
+var app     = express();
 
 var bot = new TelegramBot(config.token, config.options);
 
@@ -15,4 +17,16 @@ bot.getMe().then(function (me) {
 
 bot.on('message', function (message) {
   console.log(message);
+});
+
+
+app.set('port', (process.env.PORT || 5000));
+
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
 });
