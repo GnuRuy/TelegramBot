75a76d991f02a15dd0deabca98709689429b890c
Rodolfo Silva <contato@rodolfosilva.com>
2016-06-13 14:48:12 -0300

    Removendo texto

--- commands/formula_ap3.js
+++ commands/formula_ap3.js
@@ -2,6 +2,6 @@ var sprintf = require('sprintf-js').sprintf;
 
 module.exports = function (bot) {
   bot.onText(/qual.*(?:é|e)?.*a.*f(?:ó|o)rmula.*da.*ap3/i, function (msg, match) {
-    bot.sendMessage(msg.chat.id, sprintf("@%s a fórmula é bem simples: \n\n`7,0 na ap2 6,5 na ap2 (50 - ((AP1 + AP2) * 3)) / 4`", msg.from.username));
+    bot.sendMessage(msg.chat.id, sprintf("@%s a fórmula é bem simples: \n\n`(50 - ((AP1 + AP2) * 3)) / 4`", msg.from.username));
   });
 };
