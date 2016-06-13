26d059f0f43da6d4ddc9243b7d06bdcc99c60071
Rodolfo Silva <contato@rodolfosilva.com>
2016-06-13 14:46:25 -0300

    Correções na ortografia e atualização da fórmula

--- commands/formula_ap3.js
+++ commands/formula_ap3.js
@@ -1,7 +1,7 @@
 var sprintf = require('sprintf-js').sprintf;
 
 module.exports = function (bot) {
-  bot.onText(/qual.*(?:é|e).*a.*f(?:ó|o)rmula.*da.*ap3/i, function (msg, match) {
-    bot.sendMessage(msg.chat.id, sprintf("@%s a formulá é bem simples: \n\n`100 / ( (AP1 + AP2) * 3 )`", msg.from.username));
+  bot.onText(/qual.*(?:é|e)?.*a.*f(?:ó|o)rmula.*da.*ap3/i, function (msg, match) {
+    bot.sendMessage(msg.chat.id, sprintf("@%s a fórmula é bem simples: \n\n`7,0 na ap2 6,5 na ap2 (50 - ((AP1 + AP2) * 3)) / 4`", msg.from.username));
   });
 };
--- commands/formula_media.js
+++ commands/formula_media.js
@@ -1,7 +1,7 @@
 var sprintf = require('sprintf-js').sprintf;
 
 module.exports = function (bot) {
-  bot.onText(/qual.*(?:é|e).*a.*f(?:ó|o)rmula.*da.*m(?:é|e)dia/i, function (msg, match) {
-    bot.sendMessage(msg.chat.id, sprintf("@%s a formulá é bem simples: \n\n`abs((AP1 + AP2) * 3 + AP3 * 4 / 10)`", msg.from.username));
+  bot.onText(/qual.*(?:é|e)?.*a.*f(?:ó|o)rmula.*da.*m(?:é|e)dia/i, function (msg, match) {
+    bot.sendMessage(msg.chat.id, sprintf("@%s a fórmula é bem simples: \n\n`abs((AP1 + AP2) * 3 + AP3 * 4 / 10)`", msg.from.username));
   });
 };
